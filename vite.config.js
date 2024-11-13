88932a7222e669a794c5208050346e52c0e1359f
shivmSingh1 <shivams7380@gmai.com>
2024-11-13 07:04:46 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-	base: '/minorProject/', // Update with your repo name
+	// base: '/minorProject/', // Update with your repo name
 })
