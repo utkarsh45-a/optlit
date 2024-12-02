054309dcf118f991a05d9c507426255108b1c70f
shivmSingh1 <shivams7380@gmai.com>
2024-12-03 00:24:10 +0530

    updated

--- backend/connectDb.js
+++ backend/connectDb.js
@@ -1,8 +1,14 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+const dbUrl = process.env.ATLAS_URL;
 
 const connectDB = async () => {
 	try {
-		await mongoose.connect("mongodb://127.0.0.1:27017/optlit")
+		// await mongoose.connect("mongodb://127.0.0.1:27017/optlit")
+		await mongoose.connect(dbUrl);
 		console.log("connected to db")
 	} catch (error) {
 		console.log(`connection faild ${error}`)
